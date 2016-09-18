91ac61161985fc36ad7ae3a9c4643c6275c7cfd0
Sanjiv <sanjiv1994@gmail.com>
2016-09-18 19:17:47 +0530

    Initial Commit

---
+++ NoiseReduction/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  NoiseReduction
+//
+//  Created by Sanjiv Soni on 18/09/16.
+//  Copyright © 2016 Sanjiv Soni. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
